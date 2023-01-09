d93f03a9862e4cbcb9b35cb6d3ea0d61d6142a4f
AlvaroRuCh <alvaroch250@gmail.com>
2023-01-09 08:04:53 +0100

    Fix light theme issues

--- src/services/changeTheme.js
+++ src/services/changeTheme.js
@@ -4,5 +4,8 @@ export default function changeTheme() {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.add("dark")
     }
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        document.body.classList.add("light")
+    }
 }
 
