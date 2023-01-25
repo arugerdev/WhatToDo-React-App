af52dbcc308ca3c0d23d5e486b9e110a97906be5
AlvaroRuCh <64039132+arugerdev@users.noreply.github.com>
2023-01-25 10:14:46 +0100

    Update GitHub url

--- src/components/SocialNetworks/index.js
+++ src/components/SocialNetworks/index.js
@@ -7,7 +7,7 @@ import linkedin from "images/linkedin.png"
 export default function SocialNetworks() {
     return (
         <div className="socialNets">
-            <a rel="noreferrer" className="socialNet-Button" href="https://github.com/AlvaroRuCh" target='_blank'><img alt="github" src={gitHub}></img> </a>
+            <a rel="noreferrer" className="socialNet-Button" href="https://github.com/arugerdev" target='_blank'><img alt="github" src={gitHub}></img> </a>
             <a rel="noreferrer" className="socialNet-Button" href="https://instagram.com/aruger_dev" target='_blank'><img alt="instagram" src={instagram}></img> </a>
             <a rel="noreferrer" className="socialNet-Button" href="https://linkedin.com/in/aruger" target='_blank'><img alt="linkedin" src={linkedin}></img> </a>
         </div>
